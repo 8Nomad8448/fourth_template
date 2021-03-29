de241783d2d134cf7678899b3eee40331bc0fab2
8nomad8448 <VadymTseiko@gmail.com>
2021-03-29 10:22:14 +0300

    finished fourth template

--- js/main.js
+++ js/main.js
@@ -1,62 +1,100 @@
 $(document).ready(function(){
-    var owl = $("#owl-slider");
-  owl.owlCarousel({
-  	loop: true,
-  	margin: 0,
-  	items: 1,
-  	center: true,
-  	dots: false,
-  	nav: true,
-    touchDrag  : false,
-    mouseDrag  : false,
-    autoplay:true,
-    autoplayTimeout:4000,
-    autoplayHoverPause:false,
-    animateIn: "fadeIn",
-    animateOut: "fadeOut",
-  });
-
-    var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-    var btn = document.getElementById("toggleModal");
-
-// Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-        $("body").addClass("modal-open");
-    }
-
-// When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-        $("body").removeClass("modal-open");
-    }
-
-// When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-            $("body").removeClass("modal-open");
+    function screenClass() {
+        var count = $("#owl-slider > div").length;
+        if($(window).innerWidth() < 850 || count > 3) {
+            var owl = $("#owl-slider");
+            owl.owlCarousel({
+                loop: true,
+                margin: 0,
+                items: 3,
+                responsive : {
+                    0 : {
+                        items: 1
+                    },
+                    600 : {
+                        items: 2,
+                    },
+                    850 : {
+                        items: 3,
+                    }
+                },
+                center: false,
+                dots: true,
+                nav: false,
+                touchDrag  : true,
+                mouseDrag  : true,
+                autoplay:true,
+                autoplayTimeout:4000,
+                autoplayHoverPause:true,
+                animateIn: "fadeIn",
+                animateOut: "fadeOut"
+            });
+        } if (count == 2){
+            $('#owl-slider').removeClass('owl-carousel');
+            $('#owl-slider').removeClass('owl-theme');
+            $('#owl-slider').addClass('row');
+            $('#owl-slider > .item').addClass('col-md-6');
+        }if (count == 1){
+            $('#owl-slider').removeClass('owl-carousel');
+            $('#owl-slider').removeClass('owl-theme');
+            $('#owl-slider').addClass('row');
+            $('#owl-slider > .item').addClass('col-md-12');
+        } else {
+            $('#owl-slider').removeClass('owl-carousel');
+            $('#owl-slider').removeClass('owl-theme');
+            $('#owl-slider').addClass('row');
+            $('#owl-slider > .item').addClass('col-md-4');
         }
     }
-
+    screenClass();
+    var owl = $("#owl-feedback");
+    owl.owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        center: true,
+        dots: true,
+        nav: false,
+        touchDrag  : true,
+        mouseDrag  : true,
+        autoplay:false,
+        autoplayTimeout: 10000,
+        autoplayHoverPause:true,
+        animateIn: "fadeIn",
+        animateOut: "fadeOut"
+    });
+    var owl = $("#owl-mentors");
+    owl.owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        center: true,
+        dots: true,
+        nav: false,
+        touchDrag  : true,
+        mouseDrag  : true,
+        autoplay:true,
+        autoplayTimeout: 10000,
+        autoplayHoverPause: true,
+        animateIn: "fadeIn",
+        animateOut: "fadeOut"
+    });
 // Validation form functions
     $("#submit").click(function() {
         let name= $(".name").val();
+        let suname= $(".suname").val();
         let telephone= $(".phone").val();
+        let interrest = $(".interested_in").val();
         let email= $(".email").val();
         let nameReg = /^[a-zA-Z\u0401\u0451\u0410-\u044f]{2,40}$/;
         let regMail = /^\S+@\S+\.\S+$/;
         let regPhone = /^[0-9\-\+]{9,15}$/;
         let errorname = [];
+        let errorsuname = [];
         let errorphone = [];
+        let errorinterest = [];
         let errormail = [];
         if (name==null || name=="" || name.search(nameReg)){
-            $(".name").attr("placeholder", "Поле обязательно до заполнения");
             $(".name").addClass("novalid_input");
             $(".close").removeClass("sended");
             errorname[0] = 1;
@@ -65,8 +103,6 @@ $(document).ready(function(){
             errorname[0] = 0;
         }
         if(telephone.search(regPhone)){
-
-            $(".phone").attr("placeholder", "Поле обязательно до заполнения");
             $(".phone").addClass("novalid_input");
             $(".close").removeClass("sended");
             errorphone[0] = 1;
@@ -74,14 +110,28 @@ $(document).ready(function(){
             $(".phone").removeClass("novalid_input");
             errorphone[0] = 0;
         }
+        if(suname==null || suname=="" || suname.search(nameReg)){
+            $(".suname").addClass("novalid_input");
+            $(".suname").removeClass("sended");
+            errorsuname[0] = 1;
+        } else {
+            $(".suname").removeClass("novalid_input");
+            errorsuname[0] = 0;
+        }
         if (email.search(regMail)) {
-            $(".email").attr("placeholder", "Поле обязательно до заполнения");
             $(".email").addClass("novalid_input");
             $(".close").removeClass("sended");
             errormail[0] = 1;
         } else {
             $(".inpMail").removeClass("novalid_input");
             errormail[0] = 0;
+        }if(interrest==null || interrest=="" || interrest.search(nameReg)){
+            $(".interested_in").addClass("novalid_input");
+            $(".interrest").removeClass("sended");
+            errorinterest[0] = 1;
+        } else {
+            $(".interested_in").removeClass("novalid_input");
+            errorinterest[0] = 0;
         }
         if (errorname[0] != 1 && errorphone[0] != 1 && errormail[0] != 1) {
             $(".email").removeClass("novalid_input");
